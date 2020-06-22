e00a7213f08662b87bf1d430e233da7f80c0529c
michaelmulatya@gmail.com <msechu2011>
2020-06-22 14:43:34 +0300

    added fragments for both post & prepaid

--- app/src/main/java/com/bluefox/kenyapowerbill/MainActivity.java
+++ app/src/main/java/com/bluefox/kenyapowerbill/MainActivity.java
@@ -1,8 +1,12 @@
 package com.bluefox.kenyapowerbill;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TabLayout tabLayout = findViewById(R.id.tablayout);
+        TabItem prepaid = findViewById(R.id.prepaid);
+        TabItem postpaid = findViewById(R.id.postpaid);
+        final ViewPager viewPager = findViewById(R.id.viewPager);
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
