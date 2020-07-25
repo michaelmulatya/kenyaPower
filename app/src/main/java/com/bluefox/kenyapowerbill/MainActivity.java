763e59a17982fa2cca413be93a59d7bac73bcff7
michaelmulatya@gmail.com <msechu2011>
2020-07-25 13:19:39 +0300

    added fragments for both post & prepaid

--- app/src/main/java/com/bluefox/kenyapowerbill/MainActivity.java
+++ app/src/main/java/com/bluefox/kenyapowerbill/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 
@@ -14,13 +17,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+
+            }
+        });
+
+        // TabLayout and Tab Items initialization
         TabLayout tabLayout = findViewById(R.id.tablayout);
         TabItem prepaid = findViewById(R.id.prepaid);
         TabItem postpaid = findViewById(R.id.postpaid);
+
+        //PageAdapted initialization and code
         final ViewPager viewPager = findViewById(R.id.viewPager);
         PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
         viewPager.setAdapter(pagerAdapter);
 
+        //TabLayout selection listener
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
@@ -37,5 +51,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
     }
 }
--- app/src/main/java/com/bluefox/kenyapowerbill/PrePaidFragment.java
+++ app/src/main/java/com/bluefox/kenyapowerbill/PrePaidFragment.java
@@ -1,12 +1,40 @@
-package com.bluefox.kenyapowerbill;
+ package com.bluefox.kenyapowerbill;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdLoader;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.formats.MediaView;
+import com.google.android.gms.ads.formats.UnifiedNativeAd;
+import com.google.android.gms.ads.formats.UnifiedNativeAdView;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +51,12 @@ public class PrePaidFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private Spinner tariff;
+    private EditText mEtAmount;
+    private Button mBttnCalculate;
+    private UnifiedNativeAd nativeAd;
+
 
     public PrePaidFragment() {
         // Required empty public constructor
@@ -53,12 +87,167 @@ public class PrePaidFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pre_paid, container, false);
+        View view = inflater.inflate(R.layout.fragment_pre_paid, container, false);
+
+        //populate the spinner with an array of strings
+        tariff = view.findViewById(R.id.prepaid_tariff);
+        mEtAmount =view.findViewById(R.id.etAmount);
+        mBttnCalculate = view.findViewById(R.id.btnCalculate);
+        refreshAd(view);
+
+        mBttnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                refreshAd(view);
+                int position = tariff.getSelectedItemPosition();
+                if(!mEtAmount.getText().toString().isEmpty()){
+                    int amount = Integer.parseInt(mEtAmount.getText().toString());
+                    if(amount >249){
+                        Log.i("value for spinner","position "+position);
+                        String url = "http://36ebef90c7c7.ngrok.io/prepaid";
+                        String postBody = "{\"amount\":" + amount + ",\"tariff\":\"" + position + "\"}\n";
+                        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+                        RequestBody body = RequestBody.create(JSON, postBody);
+                        Request request = new Request.Builder()
+                                .url(url)
+                                .post(body)
+                                .build();
+                        OkHttpClient client = new OkHttpClient();
+                        client.newCall(request).enqueue(new Callback() {
+                            @Override
+                            public void onFailure(Request request, IOException e) {
+                                e.printStackTrace();
+                                Toast.makeText(getContext(), "An error occurred, try again", Toast.LENGTH_SHORT).show();
+
+                            }
+
+                            @Override
+                            public void onResponse(Response response) throws IOException {
+                                if(!response.isSuccessful()){
+                                    throw new IOException("Unexpected Code "+ response);
+                                }else {
+                                    Log.i("response from server", " "+response.body().toString());
+                                    
+
+                                }
+
+                            }
+                        });
+
+
+
+
+                    }else {
+                        mEtAmount.setError("Amount has to be 250 or above");
+                    }
+
+
+
+                }else{
+                    mEtAmount.setError("Amount cannot be empty");
+                }
+
+
+            }
+
+        });
+
+
+
+        return view;
+    }
+
+    private void refreshAd(View view) {
+        AdLoader.Builder builder = new AdLoader.Builder(getContext(), getString(R.string.native_ad_unit_id));
+        builder.forUnifiedNativeAd(new UnifiedNativeAd.OnUnifiedNativeAdLoadedListener() {
+            @Override
+            public void onUnifiedNativeAdLoaded(UnifiedNativeAd unifiedNativeAd) {
+                if (nativeAd != null)
+                    nativeAd = unifiedNativeAd;
+                CardView cardView = view.findViewById(R.id.ad_cardview);
+
+                UnifiedNativeAdView adView = (UnifiedNativeAdView) getLayoutInflater().inflate(R.layout.native_ad_layout, null);
+
+                populateNativeAd(unifiedNativeAd, adView);
+                cardView.removeAllViews();
+                cardView.addView(adView);
+
+            }
+
+            private void populateNativeAd(UnifiedNativeAd unifiedNativeAd, UnifiedNativeAdView adView) {
+                adView.setHeadlineView(view.findViewById(R.id.ad_headline));
+                adView.setAdvertiserView(view.findViewById(R.id.ad_advertiser));
+                adView.setBodyView(view.findViewById(R.id.ad_body_text));
+                adView.setStarRatingView(view.findViewById(R.id.star_rating));
+                adView.setMediaView((MediaView) adView.findViewById(R.id.media_view));
+                adView.setCallToActionView(view.findViewById(R.id.ad_call_to_action));
+                adView.setIconView(view.findViewById(R.id.adv_icon));
+
+                adView.getMediaView().setMediaContent(nativeAd.getMediaContent());
+                ((TextView)adView.getHeadlineView()).setText(nativeAd.getHeadline());
+
+                if (nativeAd.getBody() == null) {
+                    adView.getBodyView().setVisibility(View.INVISIBLE);
+                } else {
+                    ((TextView) adView.getBodyView()).setText(nativeAd.getBody());
+                    adView.getBodyView().setVisibility(View.VISIBLE);
+
+                }
+
+                if (nativeAd.getAdvertiser() == null) {
+                    adView.getAdvertiserView().setVisibility(View.INVISIBLE);
+                } else {
+                    ((TextView) adView.getAdvertiserView()).setText(nativeAd.getAdvertiser());
+                    adView.getAdvertiserView().setVisibility(View.VISIBLE);
+                }
+                if (nativeAd.getStarRating() == null) {
+                    adView.getStarRatingView().setVisibility(View.INVISIBLE);
+                } else {
+                    ((RatingBar) adView.getStarRatingView()).setRating(nativeAd.getStarRating().floatValue());
+                    adView.getStarRatingView().setVisibility(View.VISIBLE);
+                }
+
+                if (nativeAd.getIcon() == null) {
+                    adView.getIconView().setVisibility(View.GONE);
+                } else {
+                    ((ImageView) adView.getIconView()).setImageDrawable(nativeAd.getIcon().getDrawable());
+                    adView.getIconView().setVisibility(View.VISIBLE);
+                }
+                if (nativeAd.getCallToAction() == null) {
+                    adView.getCallToActionView().setVisibility(View.INVISIBLE);
+                } else {
+                    adView.getCallToActionView().setVisibility(View.VISIBLE);
+                    ((Button) adView.getCallToActionView()).setText(nativeAd.getCallToAction());
+                }
+
+                adView.setNativeAd(nativeAd);
+            }
+        });
+
+        AdLoader adLoader = builder.withAdListener(new AdListener(){
+            @Override
+            public void onAdClosed() {
+                Log.i("ad failed","native Ad failed");
+                super.onAdClosed();
+            }
+        }).build();
+        adLoader.loadAd(new AdRequest.Builder().build());
+    }
+
+    @Override
+    public void onDestroy() {
+        if (nativeAd!=null)
+            nativeAd.destroy();
+        super.onDestroy();
     }
 }
